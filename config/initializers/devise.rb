b4143b0edab571d8e5bb8b5ece021180f10967bd
giorgi01 <giorgi.tamarashvili.1@btu.edu.ge>
2020-02-06 22:36:29 +0400

    Added Email confirmation & SignUp page

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -59,7 +59,7 @@ Devise.setup do |config|
   # modifying a user and when used to authenticate or find a user. Default is :email.
   config.strip_whitespace_keys = [:email]
 
-  config.mailer_sender = 'hello@example.com'
+  config.mailer_sender = 'donot-reply@photo-app-gt.herokuapp.com'
 
   # Tell if authentication through request.params is enabled. True by default.
   # It can be set to an array that will enable params authentication only for the
